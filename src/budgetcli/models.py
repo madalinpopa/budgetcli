eb212714507cfe5e71e5d810c2658c2fb55abad1
Madalin Popa <github@madalintech.com>
2022-07-16 23:00:25 +0300

    add tests for models

--- src/budgetcli/models.py
+++ src/budgetcli/models.py
@@ -1,8 +1,11 @@
 """
 main.py
 """
+import datetime
 from enum import Enum
 from typing import NamedTuple
+from decimal import Decimal
+from dataclasses import dataclass
 
 
 class AccountType(Enum):
@@ -19,7 +22,7 @@ class AccountType(Enum):
     CASH: int = 2
 
 
-class Accounts:
+class Account:
     """Represents a budget account
 
     The user can add transactions on a specific account.
@@ -30,14 +33,51 @@ class Accounts:
 
     acc_type: AccountType = AccountType.CHECKING
 
-    def __init__(self, acc_name: str, acc_ammount: int = 0) -> None:
+    def __init__(self, acc_name: str, acc_ammount: Decimal = Decimal(0)) -> None:
         self.acc_name = acc_name
         self.acc_ammount = acc_ammount
 
 
-class Transactions(NamedTuple):
+class Activity(NamedTuple):
+    """Represent an activity over a category
+
+    This class tracks the activities over a category budget
+    """
+
+    available: int = 0
+
+
+class Category(NamedTuple):
+    """Represents a budget category
+
+    The category is used unde add new transaction.
+    The transaction should be for a specific account for one category.
+    """
+
+    name: str
+    activity: Activity = Activity
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}"
+
+
+@dataclass
+class Transaction:
     """Represents a transaction entry
 
     A transaction ca be added for many accounts.
     Also, a transaction can be added for a specific category
     """
+
+    account: Account
+    category: Category
+    payee: str = ""
+    inflow: Decimal = Decimal(0)
+    outflow: Decimal = Decimal(0)
+    date: datetime.date = datetime.date.today()
+
+    def __post_init__(self):
+        if self.inflow:
+            self.account.acc_ammount += self.inflow
+        if self.outflow:
+            self.account.acc_ammount -= self.outflow
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,15 +1,26 @@
 """
 Tests for accounts classes.
 """
-from budgetcli.models import Accounts, AccountType
+import datetime
+import pytest
+
+from budgetcli.models import Account, AccountType, Category, Transaction
 
 
-class TestAccountModel:
+@pytest.fixture
+def get_categories() -> list[Category]:
+    """Return a list of categories"""
+    names = ["Fun", "Groceries", "Internet"]
+    categories = [Category(name) for name in names]
+    return categories
+
+
+class TestAccountsModel:
     """Contains all the tests for account model"""
 
     def test_account_name(self):
         """Test the instace of the account class"""
-        account = Accounts(acc_name="cash")
+        account = Account(acc_name="cash")
         assert account.acc_name == "cash"
 
     def test_account_type(self):
@@ -17,7 +28,7 @@ class TestAccountModel:
 
         It should have a default value..
         """
-        account = Accounts("cash")
+        account = Account("cash")
         assert account.acc_type == AccountType.CHECKING
 
     def test_account_ammount(self):
@@ -25,5 +36,41 @@ class TestAccountModel:
 
         It should have a value of 0 if not set.
         """
-        account = Accounts("cash")
+        account = Account("cash")
         assert account.acc_ammount == 0
+
+
+class TestCategoriesModel:
+    """Contains all the tests for the category model"""
+
+    def test_category_name(self):
+        """Test the category name"""
+        category = Category(name="Shoping")
+        assert category.name == "Shoping"
+
+    def test_category_empty_name(self):
+        """Test if category is created without providing name
+        It should raise TypeError
+        """
+        with pytest.raises(TypeError):
+            Category()
+
+    def test_category_representation(self):
+        """Test the string repr"""
+        category = Category(name="Shoping")
+        assert repr(category) == "Category"
+
+
+class TestTransactionsModel:
+    """Contains all the tests for the transaction model"""
+
+    def test_create_new_transaction(self):
+        """Test creating new transaction"""
+
+        account = Account("Wallet", 200)
+        category = Category(name="Groceries")
+        Transaction(account=account, category=category, inflow=50)
+        Transaction(account=account, category=category, outflow=20)
+        Transaction(account=account, category=category, inflow=10)
+
+        assert account.acc_ammount == 240
